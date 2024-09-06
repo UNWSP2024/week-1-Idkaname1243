80c0925243b40227c37e91365c6e20cae4c7e9cc
Idkaname1243 <anderskjelland7@gmail.com>
2024-09-06 08:23:32 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,6 +1,6 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('seest thou a man diligent in his business? he shall stand before kings; He shall not stand before mean men.')
+    print("seest thou a man diligent in his business? he shall stand before kings; He shall not stand before mean men.")
     
 
 proverbs_22_29()
