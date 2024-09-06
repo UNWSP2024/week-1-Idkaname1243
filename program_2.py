6664c1c0a96bdd492e130269a3f46833366a554a
Idkaname1243 <anderskjelland7@gmail.com>
2024-09-06 09:23:28 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,6 +1,6 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('Do you see someone skilled in their work? They will serve before kings; they will not serve before officials of low rank.')
+    print('Proverbs 22:29 Do you see someone skilled in their work? They will serve before kings; they will not serve before officials of low rank.')
     
 
 proverbs_22_29()
